44a0c6cc4937d3d8fb47ca403f2839958404e34d
Yann Zerlaut <yann.zerlaut@gmail.com>
2023-04-13 14:25:17 +0200

    adding annot

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ from .src.inset import *
 from .src.adjust_plots import *
 from .src.legend import *
 from .src.export import *
+from .src.annotations import *
 
 pylab.style.use(os.path.join(pathlib.Path(__file__).resolve().parent,
                              'style-sheets', 'manuscript_style.py'))
