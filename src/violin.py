4b5fdcaace4150fbb5313ea2a2293fdea6ce30f8
Yann Zerlaut <yann.zerlaut@gmail.com>
2023-05-31 15:22:02 +0200

    dealing with multiple violin plots

--- src/violin.py
+++ src/violin.py
@@ -2,15 +2,22 @@ import numpy as np
 import matplotlib.pylab as plt
 
 def violin(data,
-        ax=None,
-        COLORS=None,
-        title='',
-        figsize=(1.5, 1),
-        legend=None):
+           X=None,
+           ax=None,
+           COLORS=None,
+           title='',
+           figsize=(1.5, 1),
+           legend=None):
 
     """
     return fig, ax
     """
+
+    data = np.array(data)
+    if (X is None) and (len(data.shape)>1):
+        X = np.arange(data.shape[1])
+    elif (X is None):
+        X = [0]
 
     # getting or creating the axis
     if ax is None:
@@ -18,9 +25,10 @@ def violin(data,
     else:
         fig = plt.gcf()
 
-    ax.plot(1+np.arange(data.shape[1]), data.mean(axis=0), 'ko', ms=1)
+    ax.plot(X, np.array(data.mean(axis=0)), 'ko', ms=1)
     violin_parts = ax.violinplot(data,
-                                 quantiles=[[0.25,0.5,0.75] for i in range(data.shape[1])],
+                                 positions=X,
+                                 quantiles=[[0.25,0.5,0.75] for i in range(len(X))],
                                  showextrema=False)
 
     if COLORS is not None:
@@ -41,8 +49,9 @@ if __name__=='__main__':
     import sys
     sys.path.append('./')
 
-    data = np.random.randn(100, 4)
-
+    data = np.random.randn(100)#, 4)
     #plotting
     fig, ax = violin(data, COLORS=[plt.cm.tab10(i) for i in range(4)])
+    data = np.random.randn(100)#, 4)
+    violin(data, X=[1], COLORS=[plt.cm.tab10(2)], ax=ax)
     plt.show()
