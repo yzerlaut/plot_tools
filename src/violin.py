1f6e5a4f5fa9e755c230da95da4466445defb6ad
Yann Zerlaut <yann.zerlaut@gmail.com>
2023-06-20 14:20:04 +0200

    update violin

--- src/violin.py
+++ src/violin.py
@@ -5,6 +5,11 @@ def violin(data,
            X=None,
            ax=None,
            COLORS=None,
+           showmeans=True,
+           showextrema=False,
+           showmedians=False,
+           quantiles=[0.25, 0.5, 0.75],
+           points=100,
            title='',
            figsize=(1.5, 1),
            legend=None):
@@ -25,11 +30,14 @@ def violin(data,
     else:
         fig = plt.gcf()
 
-    ax.plot(X, np.array(data.mean(axis=0)), 'ko', ms=1)
+    if showmeans:
+        ax.plot(X, np.array(data.mean(axis=0)), 'ko', ms=1)
     violin_parts = ax.violinplot(data,
                                  positions=X,
-                                 quantiles=[[0.25,0.5,0.75] for i in range(len(X))],
-                                 showextrema=False)
+                                 quantiles=[quantiles for i in range(len(X))],
+                                 showextrema=False,
+                                 showmedians=showmedians,
+                                 points=points)
 
     if COLORS is not None:
         for pc, color in zip(violin_parts['bodies'], COLORS):
@@ -53,5 +61,5 @@ if __name__=='__main__':
     #plotting
     fig, ax = violin(data, COLORS=[plt.cm.tab10(i) for i in range(4)])
     data = np.random.randn(100)#, 4)
-    violin(data, X=[1], COLORS=[plt.cm.tab10(2)], ax=ax)
+    violin(data, X=[1], COLORS=[plt.cm.tab10(2)], ax=ax, points=1000)
     plt.show()
