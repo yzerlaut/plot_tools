9a148f8c7710935c60666f15e707e1756ff3a428
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-03-25 19:47:48 +0100

    importing all colormaps

--- src/colors.py
+++ src/colors.py
@@ -1,6 +1,4 @@
-from matplotlib.cm import viridis, viridis_r, copper, copper_r, cool, jet,\
-    PiYG, binary, binary_r, bone, Pastel1, Pastel2, Paired, Accent, Dark2, Set1, Set2,\
-    Set3, tab10, tab20, tab20b, tab20c
+from matplotlib.cm import *
 
 import matplotlib.colors as mpl_colors
 
