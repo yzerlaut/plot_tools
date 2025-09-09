f45b763657e023bb25fb2f94b66f0c378157aba9
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-09-09 17:53:42 +0200

    update nb

--- tests/Notebook.py
+++ tests/Notebook.py
@@ -1,24 +1,9 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.14.0
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 # %%
 import sys
 
 sys.path.append('../..')
 import plot_tools as pt
-pt.set_style('dark-notebook')
+pt.set_style('dark')
 import matplotlib.pylab as plt
 import numpy as np
 
